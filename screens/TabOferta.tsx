e9f80ef5f2bcb1f45c7cb05a3d3ce1f477fc43ca
Enzo Crisóstomo <enzo.crisostomo2000@gmail.com>
2022-09-19 20:46:37 -0300

    remover component antigo

--- screens/TabOferta.tsx
+++ screens/TabOferta.tsx
@@ -8,7 +8,6 @@ import {
     TextInput,
 } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 import { CardDisciplina } from "../components/CardDisciplina";
--- types.tsx
+++ types.tsx
@@ -38,6 +38,8 @@ export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
         NativeStackScreenProps<RootStackParamList>
     >;
 
+//Tipos específicos do MW2
+
 export type CargaHorariaCurso = {
     totalMinima: Number;
     totalObrigatoria: Number;
